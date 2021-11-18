f3ada08f9fe151d9b9b8a7da88cd354b1d9db689
Mikel Sanchez <mikelsan@bu.edu>
2021-11-18 01:38:58 -0500

    change

--- 1-Registering-Accounts/src/UserPool.js
+++ 1-Registering-Accounts/src/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: "us-east-1_dm3FV28co",
-    ClientId: "78giafniv8n4t9kopp9qd1lh87"
+    UserPoolId: "us-east-2_iBzCmTA5R",
+    ClientId: "2ifdjh17k4kinmb7ddit96084p"
 }
 
 export default new CognitoUserPool(poolData);
